2b9de21fe15e03c7bf6d45fef59c842100c71422
markwisdomreeves <wisdom3510802563@gmail.com>
2020-02-15 09:58:27 -0800

    error says thumbnail not defined

--- js/script.js
+++ js/script.js
@@ -1,86 +1,91 @@
-
-// Global variables
-const searchParent = document.querySelector(".search-container");
+//global variables
 const gellaryContainer = document.querySelector("#gallery");
-// const randomuserUrl = "https://randomuser.me/api/?results=12&nat=us";
-const randomuserUrl = "https://randomuser.me/api/?nat=us&results=12"
+// const body = document.querySelector('body');
+const randomuserUrl = "https://randomuser.me/api/?results=12&nat=us"
 let userData = [];
 
 
-// fetching the data from the randomuser api section
-function randomuserData() {
-    fetch(randomuserUrl)
-     .then(response => response.json())  
-     .then(function(data) {
-          userData = [...data.results]
-          const randomUsers = userData
-          .map(user => {
-            return `
-                <div class="card">
-                    <div class="card-img-container">
-                        <img class="card-img" src=${user.picture.thumbnail} alt="profile picture">
-                    </div>
-                    <div class="card-info-container">
-                        <h3 id="name" class="card-name cap">${user.name.first} ${user.name.last}</h3>
-                        <p class="card-text">${user.email}</p>
-                        <p class="card-text cap">${user.location.city} ${user.location.state}</p>
-                    </div>
-                </div>
-            `
-         })
-         .join('');
-         gellaryContainer.insertAdjacentHTML('afterbegin', randomUsers)
-     })
-     .catch(err => console.log('failed to fetch data from api: ', err));
- }
- randomuserData()
+// // fetch data from the random user api
+// fetch(randomuserUrl)
+//   .then(res => res.json())
+//   .then(data => {
+//     userData = [...data.results];
+//     userCardsFunc(data.results);
+// })
+
+async function getData() 
+    {
+        //await the response of the fetch call
+        let response = await fetch(randomuserUrl);
+        //proceed once the first promise is resolved.
+        let data = await response.json()
+        userData = [...data.results]
+        userCardsFunc(data.results);
+        //proceed only when the second promise is resolved
+        return userData;
+    }
+//call getData function
+getData()
 
 
-// create search bar function section
-const searchBar = () => {
-    const createSearchForm = `
-        <form action="#" method="get">
-            <input type="search" id="search-input" class="search-input" placeholder="Search...">
-            <input type="submit" value="&#x1F50D;" id="search-submit" class="search-submit">
-        </form>`;
-    searchParent.insertAdjacentHTML('afterbegin', createSearchForm)
-}
-searchBar();
 
-
-// create the pupup modal of a single user when click
-const userPopUpModal = function(employee) {
-    const modalContainer = document.createElement('div');
-    modalContainer.className = '.modal-container';
-    const body = document.querySelector('body');
-    body.appendChild(modalContainer);
-        const htmlString = `
-            <div class="modal">
-                <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-                <div class="modal-info-container">
-                    <img class="modal-img" src="${employee.picture.thumbnail}" alt="profile picture">
-                    <h3 id="name" class="modal-name cap">${employee.name.first} ${employee.name.last}</h3>
-                    <p class="modal-text">${employee.email}</p>
-                    <p class="modal-text cap">${employee.location.city}</p>
-                    <hr>
-                    <p class="modal-text">${employee.phone}</p>
-                    <p class="modal-text">
-                        ${employee.location.street.number} 
-                        ${employee.location.street.name}, 
-                        ${employee.location.state} 
-                        ${employee.location.postcode} 
-                    </p>
-                    <p class="modal-text">Birthday: ${employee.dob.date}</p>
-                </div>
+//function to generate HTML
+const userCardsFunc = function(userData) {
+  for(var i = 0; i < userData.length; i++) {
+    const allCards = `<div class="card">
+            <div class="card-img-container">
+                <img class="card-img" src="${userData[i].picture.thumbnail}" alt="profile picture">
             </div>
-        `;
-    modalContainer.innerHTML = htmlString;
+            <div class="card-info-container">
+                <h3 id="name" class="card-name cap">${userData[i].name.first} ${userData[i].name.last}</h3>
+                <p class="card-text">${userData[i].email}</p>
+                <p class="card-text cap">${userData[i].location.city}, ${userData[i].location.state}</p>
+            </div>
+        </div>`;
+    gellaryContainer.insertAdjacentHTML('afterbegin', allCards) 
+  }
+  
+}
+
+
+//function to create modal window
+function createModalBox(data) {
+  let divOverlay = document.createElement('div');
+  divOverlay.className = 'modal-container';
+  const htmlString = 
+        `<div class="modal">
+            <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+            <div class="modal-info-container">
+                <img class="modal-img" src=${data.picture.thumbnail} alt="profile picture">
+                <h3 id="name" class="modal-name cap">${data.name.first} ${data.name.last}</h3>
+                <p class="modal-text">${data.email}</p>
+                <p class="modal-text cap">${data.location.city}</p>
+                <hr>
+                <p class="modal-text">${data.phone}</p>
+                <p class="modal-text">${data.location.street.number} 
+                 ${data.location.street.name}, ${data.location.city}, 
+                ${data.location.state} ${data.location.postcode}</p>
+                <p class="modal-text">Birthday: ${data.dob.date}${data.dob.date}${data.dob.date}</p>
+            </div>
+        </div>`;
+               
+    gellaryContainer.appendChild(htmlString);
+    // gellaryContainer.style.display = '';
+
     
-    // modalContainer.insertAdjacentHTML('afterbegin', htmlString)
-    const closeButton = document.querySelector('.modal-close-btn');
-    closeButton.addEventListener('click', e => {
-        userPopUpModal.remove()
-    })
+    // event to close the popup modal by clicking the close btn
+    const btnClose = document.getElementById('modal-close-btn');
+    btnClose.addEventListener('click', () => {
+        gellaryContainer.remove();
+    });
 
 }
+
+
+//event to display the modal when click on
+gellaryContainer.addEventListener('click', (event) => {
+  if (event.target.className.includes('card')) {
+    createModalBox(userData);
+  }
+});
 
